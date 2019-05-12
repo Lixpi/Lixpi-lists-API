4c217f497e7e6c4031cccf1685c6f23df7ef00d8
Nargiza Sarkulova <nargiza.sarkulova@shopify.com>
2019-05-12 14:42:18 -0400

    a mess 2

--- models/account.js
+++ models/account.js
@@ -1,36 +1,36 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+// var mongoose = require("mongoose");
+// var passportLocalMongoose = require("passport-local-mongoose");
 
-var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
-
-UserSchema.plugin(passportLocalMongoose);
-
-module.exports = mongoose.model("User", UserSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-// var passportLocalMongoose = require('passport-local-mongoose');
-
-// var Account = new Schema({
+// var UserSchema = new mongoose.Schema({
 //     username: String,
 //     password: String
 // });
 
-// Account.plugin(passportLocalMongoose);
+// UserSchema.plugin(passportLocalMongoose);
 
-// module.exports = mongoose.model('Account', Account);
+// module.exports = mongoose.model("User", UserSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var Account = new Schema({
+    username: String,
+    password: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Account', Account);
