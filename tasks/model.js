03958bb91015e729f02e6040432a2e0337c2afa6
Shelby Carter <lixpi@lixpi.com>
2020-03-08 13:30:17 -0400

    Task schema; create new task request
    Co-authored-by: Shelby Carter <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- tasks/model.js
+++ tasks/model.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
 
 let TaskSchema = new mongoose.Schema({
-    updated_ad: Date,
-    created_ad: Date,
+    key: String,
+    // project: Project,
     title: String,
     description: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    type: String,
+    status: String,
+    priority: String,
+    version: String,
+    labels: [{
+        color: String,
+        title: String
+    }],
+    assignees: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        role: String
+    }],
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    timeTracking: {
+        estimated: Number,
+        spent: Number
+    },
+    dueAt: Number,
+    timestamps: {
+        createdAt: Number,
+        updatedAt: Number
+    }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
