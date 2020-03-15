b179b32b98e7bf3940a29d84bfc7d2a0feeefd1d
Shelby Carter <lixpi@lixpi.com>
2020-03-15 15:11:24 -0400

    monot refactoring for Tasks test

--- tasks/services.test.js
+++ tasks/services.test.js
@@ -1,30 +1,40 @@
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
 const { expect } = require('chai')
 
 const Task = require('./model');
 const User = require('../users/model')
 const getTask = require('./services')
 
-beforeEach( async () => {
-    const mongoServer = new MongoMemoryServer();
-    const mongoUri = await mongoServer.getUri();
-    await mongoose.connect(mongoUri);
+let mongoServer
 
-    const mockUserId = mongoose.Types.ObjectId('5e684ebacb19f70020661f44');
-    const mockUser = await new User({ _id: mockUserId, username: 'testuser' }).save()
-    const mockTask1 = await new Task({ _id: '5e684ececb19f70020661f45', key: 'TAS-1', author: mockUserId }).save()
-    const mockTask2 = await new Task({ _id: '5e684ececb19f70020661f41', key: 'TAS-2', author: mockUserId }).save()
+before(async () => {
+    mongoServer = new MongoMemoryServer()
+    const mongoUri = await mongoServer.getUri()
+    await mongoose.connect(mongoUri)
 });
 
-afterEach(() => {
-    mongoose.disconnect();
+after(async () => {
+    await mongoose.disconnect()
+    await mongoServer.stop()
 });
 
 describe('Task CRUD operations', () => {
+    beforeEach( async () => {
+        // TODO: seed from fixtures
+        const mockUserId = mongoose.Types.ObjectId('5e684ebacb19f70020661f44');
+        const mockUser = await new User({ _id: mockUserId, username: 'testuser' }).save()
+        const mockTask1 = await new Task({ _id: '5e684ececb19f70020661f45', key: 'TAS-1', author: mockUserId }).save()
+        const mockTask2 = await new Task({ _id: '5e684ececb19f70020661f41', key: 'TAS-2', author: mockUserId }).save()
+    });
+
+    afterEach(() => {
+        // TODO: drop DB records here
+    });
+
     it('Should retrieve task by key including author', async () => {
         const task = await getTask('TAS-1')
 
-        expect(task.author.username).to.equal('testuser');
+        expect(task.author.username).to.equal('testuser')
     });
 });
