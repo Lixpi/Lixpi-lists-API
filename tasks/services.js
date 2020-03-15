b70d7767e93c769047bf060b28b469ff3a25f7a6
Nargiza Sarkulova <nargiza.sarkulova@gmail.com>
2020-03-15 12:20:14 -0400

    Set up in-memory mongodb for test
    Co-authored-by: Shelby Carter <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- tasks/services.js
+++ tasks/services.js
@@ -4,8 +4,8 @@ const Task = require('./model');
 const User = require('../users/model')
 
 module.exports = async function getTask (key) {
-    const task = await Task.findOne({key}).exec()
-    const user = await User.findById(task.author).exec()
-    task.author = user
+    const task = await Task.findOne({key}).
+    populate('author').
+    exec()
     return task
 };
