ce8389fa946baf2b4d2319d9d484d63a3112fde5
Nargiza Sarkulova <nargiza.sarkulova@gmail.com>
2020-03-19 20:43:11 -0400

    Create task and tests
    Co-authored-by: Shelby Carter <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- tasks/services.test.js
+++ tasks/services.test.js
@@ -29,26 +29,27 @@ describe('Task CRUD operations', () => {
         }
         // TODO: seed from fixtures
         const mockUserId = mongoose.Types.ObjectId('5e684ebacb19f70020661f44');
-        const mockUser = await new User({ _id: mockUserId, username: 'testuser' }).save()
-        const mockTask1 = await new Task({ _id: '5e684ececb19f70020661f45', key: 'TAS-1', author: mockUserId }).save()
-        const mockTask2 = await new Task({ _id: '5e684ececb19f70020661f41', key: 'TAS-2', author: mockUserId }).save()
+        this.mockUser = await new User({ _id: mockUserId, username: 'testuser' }).save()
+        this.mockTask1 = await new Task({ _id: '5e684ececb19f70020661f45', key: 'TAS-1', author: mockUserId }).save()
+        this.mockTask2 = await new Task({ _id: '5e684ececb19f70020661f41', key: 'TAS-2', author: mockUserId }).save()
     });
 
     afterEach( async () => {
     });
 
     it('Should retrieve task by key including author', async () => {
-        const task = await getTask('TAS-1')
+        const actualTask = await getTask('TAS-1')
 
-        expect(task.author.username).to.equal('testuser')
+        expect(actualTask.equals(this.mockTask1)).to.be.true;
     });
 
     it('Should retrieve tasks including author', async () => {
-        const tasks = await getTasks()
+        const actualTasks = await getTasks()
+        const expectedTasks = [this.mockTask1, this.mockTask2]
         
-        expect(tasks.length).to.equal(2)
-        tasks.forEach((task) => {
-            expect(task.author.username).to.equal('testuser')
+        expect(actualTasks.length).to.equal(2)
+        actualTasks.forEach((actualTask, i) => {
+            expect(actualTask.equals(expectedTasks[i])).to.be.true;
         });
     });
 
@@ -70,13 +71,29 @@ describe('Task CRUD operations', () => {
                     title: 'Label Title 2'
                 }
             ],
-            author: mockUser._id,
+            author: this.mockUser._id,
             timeTracking: {
                 estimated: 500,
                 spent: 200
             },
             dueAt: 1583687295554
         }
-        const task = await createTask(newTaskData)
+        const currentTimestamp = 1584660793927
+        const actualTask = await createTask(newTaskData, currentTimestamp)
+        const expectedTask = new Task(
+            Object.assign({},
+                { _id: actualTask._id },
+                { key: 'KEY-1' },
+                newTaskData,  
+                { 
+                    timestamps: {
+                        createdAt: currentTimestamp,
+                        updatedAt: currentTimestamp
+                    }
+                },
+                { author: this.mockUser }
+            )
+        )
+        expect(actualTask.equals(expectedTask)).to.be.true;
     });
 });
