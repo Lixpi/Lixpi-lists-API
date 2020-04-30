8445dbb4288166e7ff65e242091b3a86e53889b4
Nargiza Sarkulova <nargiza.sarkulova@gmail.com>
2020-04-30 19:58:31 -0400

    Started using PostgreSQL as DB
    Co-authored-by: Anton Iskandiarov <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- routes/register.js
+++ routes/register.js
@@ -1,23 +1,35 @@
 'use strict';
 
-const User = require("../users/model")
+const Bluebird = require('bluebird');
+const { User } = require("../users/model")
 
-module.exports.post = (req, res) => {
-    User.register({username: req.body.username, active: false}, req.body.password, function(err, user) {
-        if (err) {
-            res.json({
-                success: false,
-                message: "Your account could not be saved. Error: ",
-                err
-            })
-        }
-        User.authenticate()(req.body.username, req.body.password, function(err, result) {
-            if (!result) {
-                res.send('Something is wrong')
-            }
-            req.login(result, (err) => {
-                return res.send('You were authenticated & logged in!');
-            })
-        });
-    });
+module.exports.post = async (req, res) => {
+
+
+    await User.create({ name: req.body.username, email: req.body.username, password: req.body.password })
+        .then(() => {
+            return res.send('User is created');
+        })
+        .catch((err) => {
+            return res.json(err)
+        })
+
+
+    // User.save({username: req.body.username, active: false}, req.body.password, function(err, user) {
+    //     if (err) {
+    //         res.json({
+    //             success: false,
+    //             message: "Your account could not be saved. Error: ",
+    //             err
+    //         })
+    //     }
+    //     // User.authenticate()(req.body.username, req.body.password, function(err, result) {
+    //     //     if (!result) {
+    //     //         res.send('Something is wrong')
+    //     //     }
+    //     //     req.login(result, (err) => {
+    //     //         return res.send('You were authenticated & logged in!');
+    //     //     })
+    //     // });
+    // });
 }
--- users/model.js
+++ users/model.js
@@ -1,11 +1,68 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const Bluebird = require('bluebird');
+const Sequelize = require('sequelize');
 
-let UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+const sequelize = require('../db/sequelize-singleton');
+
+const mappings = {
+    userId: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.DataTypes.UUIDV4,
+    },
+    name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+};
+
+const User = sequelize.define('User', mappings, {
+    indexes: [
+        {
+            name: 'user_userId_index',
+            method: 'BTREE',
+            fields: ['userId'],
+        },
+        {
+            name: 'user_email_index',
+            method: 'BTREE',
+            fields: ['email'],
+        },
+    ],
 });
 
-UserSchema.plugin(passportLocalMongoose);
+User.prototype.comparePassword = function (password) { // eslint-disable-line func-names
+    return Bluebird.resolve()
+        .then(() => bcrypt.compareSync(password, this.password))
+        .catch((err) => {
+            console.log(err);
 
-module.exports = mongoose.model('User', UserSchema);
+            return false;
+        });
+};
+
+User.beforeSave((user) => {
+    user.name = _.trim(user.name);
+
+    if ((user.previous('password') !== user.password) && (!_.isEmpty(user.password))) {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(user.password, salt);
+        user.password = hash;
+    }
+
+    return user;
+});
+
+exports.mapping = mappings;
+
+exports.User = User;
