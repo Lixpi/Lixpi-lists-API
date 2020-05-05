b99b508f81cf7a4b7d0675592435c5c5ed7fa762
Nargiza Sarkulova <nargiza.sarkulova@gmail.com>
2020-05-05 18:15:32 -0400

    Logout works
    Co-authored-by: Shelby Carter <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,13 @@
 'use strict';
 
+/**
+ * HTTP handler for sign out.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ */
 module.exports.get = (req, res) => {
-    req.logout()
+    req.logout();
     req.session.destroy()
-    res.status(401).json({result: 'Logged out!'})
-}
+    res.status(401).json({ result: 'Logged out!' })
+};
