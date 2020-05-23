f32918978077dbbc8ec8fbd75607201c2d2dc914
Nargiza Sarkulova <nargiza.sarkulova@gmail.com>
2020-05-22 22:50:34 -0400

    Added script to sync models

--- server.js
+++ server.js
@@ -24,35 +24,6 @@ app.use(cors({
     credentials: true
 }))
 
-//--------------- Models sync ---------------//
-const { User } = require('./user/model')
-const { Session } = require('./session/model')
-const { Label } = require('./label/model')
-const { Type } = require('./task/type/model')
-const { Status } = require('./task/status/model')
-const { Priority } = require('./task/priority/model')
-const { Role } = require('./role/model')
-const { Task, TaskLabel, TaskType, TaskStatus, TaskPriority, UserRole, TaskAssignee } = require('./task/model')
-
-void (async () => {
-    await User.sync({ alter: true, force:true })
-    await Session.sync({ alter: true, force:true })
-    await Task.sync({ alter: true, force:true })
-    await Label.sync({ alter: true, force:true })
-    await Type.sync({ alter: true, force:true })
-    await Status.sync({ alter: true, force:true })
-    await Priority.sync({ alter: true, force:true })
-    await Role.sync({ alter: true, force:true })
-    await UserRole.sync({ alter: true, force:true })
-    await TaskAssignee.sync({ alter: true, force:true })
-    await TaskLabel.sync({ alter: true, force:true })
-    await TaskType.sync({ alter: true, force:true })
-    await TaskStatus.sync({ alter: true, force:true })
-    await TaskPriority.sync({ alter: true, force:true })
-})
-// ()
-//------------------------------------------//
-
 const passportConfig = (passport) => {
     passport.serializeUser((user, done) => {
         done(null, user.id)
