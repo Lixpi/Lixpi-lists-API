37da4ac5f831ef8a6715b40eb32a9c5ba04e1188
anton-iskandiarov <lixpi@lixpi.com>
2020-05-31 17:46:29 -0400

    prototype for Task CRUD test
    Co-authored-by: Anton Iskandiarov <lixpi@lixpi.com>
    Co-authored-by: Nargiza Sarkulova <nargiza.sarkulova@gmail.com>

--- db/db-init.js
+++ db/db-init.js
@@ -1,19 +1,17 @@
-
 const Sequelize = require('sequelize')
 const sequelize = require('./sequelize-singleton')
-
 const { env } = require('../config/node-config-loader')
 
-let sequelizeInstance
+const { User } = require('../user/model')
+const { Session } = require('../session/model')
+const { Label } = require('../label/model')
+const { Type } = require('../task/type/model')
+const { Status } = require('../task/status/model')
+const { Priority } = require('../task/priority/model')
+const { Role } = require('../role/model')
+const { Task, TaskLabel, TaskType, TaskStatus, TaskPriority, UserRole, TaskAssignee } = require('../task/model')
 
-// const { User } = require('../user/model')
-// const { Session } = require('../session/model')
-// const { Label } = require('../label/model')
-// const { Type } = require('../task/type/model')
-// const { Status } = require('../task/status/model')
-// const { Priority } = require('../task/priority/model')
-// const { Role } = require('../role/model')
-// const { Task, TaskLabel, TaskType, TaskStatus, TaskPriority, UserRole, TaskAssignee } = require('../task/model')
+let sequelizeInstance
 
 const initDb = async () => {
     sequelizeInstance = sequelize(false)
@@ -26,6 +24,11 @@ const initDb = async () => {
 
 const dropDb = async () => {
     sequelizeInstance = sequelize(false)
+
+    sequelizeInstance.query(`SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '${env.DATABASE_NAME}'`).then(data => {
+        console.log(`Dropping all active connections for '${env.DATABASE_NAME}' database!`)
+        console.log(data)
+    })
     return sequelizeInstance.query(`DROP DATABASE IF EXISTS ${env.DATABASE_NAME}`).then(data => {
         console.log(`Congrats comrade! Database '${env.DATABASE_NAME}' was dropped!`)
         console.log(data)
@@ -34,23 +37,21 @@ const dropDb = async () => {
 }
 
 const syncModels = async () => {
-    sequelizeInstance = sequelize()
-    await sequelizeInstance.sync();
-    // sequelizeInstance.close()
-    // await User.sync({ alter: true, force:true })
-    // await Session.sync({ alter: true, force:true })
-    // await Task.sync({ alter: true, force:true })
-    // await Label.sync({ alter: true, force:true })
-    // await Type.sync({ alter: true, force:true })
-    // await Status.sync({ alter: true, force:true })
-    // await Priority.sync({ alter: true, force:true })
-    // await Role.sync({ alter: true, force:true })
-    // await UserRole.sync({ alter: true, force:true })
-    // await TaskAssignee.sync({ alter: true, force:true })
-    // await TaskLabel.sync({ alter: true, force:true })
-    // await TaskType.sync({ alter: true, force:true })
-    // await TaskStatus.sync({ alter: true, force:true })
-    // await TaskPriority.sync({ alter: true, force:true })
+    console.log(`Initializing models!`)
+    await User.sync({ force:true })
+    await Session.sync({ force:true })
+    await Task.sync({ force:true })
+    await Label.sync({ force:true })
+    await Type.sync({ force:true })
+    await Status.sync({ force:true })
+    await Priority.sync({ force:true })
+    await Role.sync({ force:true })
+    await UserRole.sync({ force:true })
+    await TaskAssignee.sync({ force:true })
+    await TaskLabel.sync({ force:true })
+    await TaskType.sync({ force:true })
+    await TaskStatus.sync({ force:true })
+    await TaskPriority.sync({ force:true })
 }
 
 module.exports = {
